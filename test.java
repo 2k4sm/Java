30f8ce1f76d2122137aafbe4530e55972d4c07e0
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-07-28 16:10:37 +0530

    test

--- test.java
+++ test.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String [] args){
+        
+        Scanner scan = new Scanner(System.in);
+        /* 
+        long SumNum1 = scan.nextInt();
+        long Number2 = scan.nextInt();
+
+        for (long i = SumNum1+1; i <= Number2;i++){
+            SumNum1+=i;
+        }
+        System.out.println(SumNum1);
+        scan.close();
+        */
+        /*
+        long SumNum1 = scan.nextInt();
+        long Number2 = scan.nextInt();
+
+        for (long i = SumNum1+1; i <= Number2;){
+            if(i%2 == 0){
+                SumNum1+=i;
+                i+=2;
+
+            }else{
+                i++;
+            }
+        }
+        System.out.println(SumNum1-1);
+        scan.close();
+        */
+
+        int Num1 = scan.nextInt();
+        int revNum = 0;
+
+        for (;Num1 > 0;){
+            revNum = revNum*10 + (Num1%10);
+            Num1 /=10;
+        }
+        System.out.println(revNum);
+
+        scan.close();
+
+        
         
     }
 }
