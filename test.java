dec1070cecdf552333ed7f6814607287e25c16f1
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-07-31 14:51:50 +0530

    reversing numbers

--- test.java
+++ test.java
@@ -2,45 +2,18 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String [] args){
-        
-        Scanner scan = new Scanner(System.in);
-        /* 
-        long SumNum1 = scan.nextInt();
-        long Number2 = scan.nextInt();
+        try (Scanner scan = new Scanner(System.in)) {
+            int b = scan.nextInt();
+            for(int i=1;i<=b;i++){
+                
+                for(int j = 1;j<=b;j++){
+                    System.out.println("This is Number:"+j);
 
-        for (long i = SumNum1+1; i <= Number2;i++){
-            SumNum1+=i;
-        }
-        System.out.println(SumNum1);
-        scan.close();
-        */
-        /*
-        long SumNum1 = scan.nextInt();
-        long Number2 = scan.nextInt();
-
-        for (long i = SumNum1+1; i <= Number2;){
-            if(i%2 == 0){
-                SumNum1+=i;
-                i+=2;
-
-            }else{
-                i++;
+                
+                }
             }
         }
-        System.out.println(SumNum1-1);
-        scan.close();
-        */
 
-        int Num1 = scan.nextInt();
-        int revNum = 0;
-
-        for (;Num1 > 0;){
-            revNum = revNum*10 + (Num1%10);
-            Num1 /=10;
-        }
-        System.out.println(revNum);
-
-        scan.close();
 
         
         
