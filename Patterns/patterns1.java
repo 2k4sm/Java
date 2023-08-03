88ca2658df68916fe2a334e64119c620d3c45ca0
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-08-03 15:03:51 +0530

    This fixes the resource leak warning

--- Patterns/patterns1.java
+++ Patterns/patterns1.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
 
 public class patterns1{
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int plength = scan.nextInt();
-        int spaces = 0;
-        for (;plength != 0;plength--){
-            for (int i = 0; i < plength;i++){
-                System.out.print("*");
+        try (Scanner scan = new Scanner(System.in)) {
+            int plength = scan.nextInt();
+            int spaces = 0;
+            for (;plength != 0;plength--){
+                for (int i = 0; i < plength;i++){
+                    System.out.print("*");
+                }
+                for (int j = 0; j < spaces;j++){
+                    System.out.print(" ");
+                }
+                for (int i = 0; i < plength;i++){
+                    System.out.print("*");
+                }
+                spaces+=2;
+                System.out.println();
+                
             }
-            for (int j = 0; j < spaces;j++){
-                System.out.print(" ");
-            }
-            for (int i = 0; i < plength;i++){
-                System.out.print("*");
-            }
-            spaces+=2;
-            System.out.println();
-            
+            scan.close();
         }
-        scan.close();
     }
 }
--- Patterns/patterns2.java
+++ Patterns/patterns2.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class patterns2 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int plength = scan.nextInt();
-        int stars = 1;
-        int spaces = 2*plength-stars-1;
-        for (;plength != 0;plength--){
-            for (int i = 0; i < stars;i++){
-                System.out.print("*");
+        try (Scanner scan = new Scanner(System.in)) {
+            int plength = scan.nextInt();
+            int stars = 1;
+            int spaces = 2*plength-stars-1;
+            for (;plength != 0;plength--){
+                for (int i = 0; i < stars;i++){
+                    System.out.print("*");
+                }
+                for (int j = 0; j < spaces;j++){
+                    System.out.print(" ");
+                }
+                for (int i = 0; i < stars;i++){
+                    System.out.print("*");
+                }
+                spaces-=2;
+                stars++;
+                System.out.println();
             }
-            for (int j = 0; j < spaces;j++){
-                System.out.print(" ");
-            }
-            for (int i = 0; i < stars;i++){
-                System.out.print("*");
-            }
-            spaces-=2;
-            stars++;
-            System.out.println();
+            scan.close();
         }
-        scan.close();
     }
 }
