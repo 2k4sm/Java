4334d32754bd2c63b1e8b483dbca3bb55a7ca997
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-08-07 15:39:21 +0530

    This implements java functions.

---
+++ functions/functions.java
@@ -0,0 +1,15 @@
+public class functions{
+    public static void main(String[] args) {
+        System.out.println(sum(1,3));
+        System.out.println(product(1,3));
+
+    }
+    
+    static int sum( int a,  int b){
+        return a+b;
+    }
+    static int product(int a, int b){
+        return a*b;
+    }
+
+}
